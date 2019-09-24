577266953e8c0eda7d4546853a79f1653184b556
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-24 00:02:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_value = 0 
   lowest_key = nil
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if lowest_value == 0 || lowest_value < 0
       lowest_value = value
       lowest_key = key
